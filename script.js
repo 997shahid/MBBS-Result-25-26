c04466dd652afdb7d05047b83e86cdc48b8c44a3
shahid hasan <hello@Shahid-Hasan.local>
2025-12-03 23:10:05 +0600

    final

--- script.js
+++ script.js
@@ -1,14 +1,15 @@
-function checkRoll()
-{
- let roll = document.getElementById("roll").value;
- if(roll == ""){
-  
-  document.getElementById("errorMsg").style.display = "flex";
-  return false;
- }
- else
- {
-    document.getElementById("errorMsg").style.display = "none";
-  return true;
- }
+function checkRoll(){
+  let roll = document.getElementById("roll").value;
+  if(roll == ""){
+    document.getElementById("errorMsg").style.display = "flex";
+    document.querySelector(".info").style.display = "none";
+        return false;
+    } else {
+          document.getElementById("errorMsg").style.display = "none";
+          document.querySelector(".info").style.display = "block";
+
+        return false;
+    }
 }
+
+
